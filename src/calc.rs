62d6605a8f8ddd2957586ef728265600b94da971
chrnigi <147508181+chrnigi@users.noreply.github.com>
2023-10-30 19:01:41 +0100

    remove pub

--- src/calc.rs
+++ src/calc.rs
@@ -1,18 +1,18 @@
 use crate::input::UserIn;
 
-pub fn add_mut_f64(num_1: &mut f64, num_2: &f64) {
+fn add_mut_f64(num_1: &mut f64, num_2: &f64) {
     *num_1 = *num_1 + *num_2;
 }
 
-pub fn subtract_mut_f64(num_1: &mut f64, num_2: &f64) {
+fn subtract_mut_f64(num_1: &mut f64, num_2: &f64) {
     *num_1 = *num_1 - *num_2;
 }
 
-pub fn mult_mut_f64(num_1: &mut f64, num_2: &f64) {
+fn mult_mut_f64(num_1: &mut f64, num_2: &f64) {
     *num_1 = *num_1 * *num_2;
 }
 
-pub fn divi_mut_f64(num_1: &mut f64, num_2: &f64) {
+fn divi_mut_f64(num_1: &mut f64, num_2: &f64) {
     *num_1 = *num_1 / *num_2;
 }
 

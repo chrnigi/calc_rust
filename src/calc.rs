ea38428774db9aced892d0dee7503e170333f4ea
chrnigi <147508181+chrnigi@users.noreply.github.com>
2023-10-30 08:51:28 +0100

    Arithmetic functions

---
+++ src/calc.rs
@@ -0,0 +1,15 @@
+pub fn add_f64(num_1: &f64, num_2: &f64) -> f64 {
+    return num_1+num_2;
+}
+
+pub fn subtract_f64(num_1: &f64, num_2: &f64) -> f64 {
+    return num_1-num_2;
+}
+
+pub fn mult_f64(num_1: &f64, num_2: &f64) -> f64 {
+    return num_1*num_2;
+}
+
+pub fn divi_f64(num_1: &f64, num_2: &f64) -> f64 {
+    return num_1/num_2;
+}

eda46efda47b3b21917e10ce88a02311c333463b
chrnigi <147508181+chrnigi@users.noreply.github.com>
2023-10-30 11:14:13 +0100

    match

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,13 @@ fn main() {
     println!("{}", x);
     let user_input = input::num_in();
     x = user_input.operand;
-    if user_input.operator == '+'{
-        calc::add_mut_f64(&mut cur_res, &x)
+
+    match user_input.operator {
+        '+' => calc::add_mut_f64(&mut cur_res, &x),
+        '-' => calc::subtract_mut_f64(&mut cur_res, &x),
+        '*' => calc::mult_mut_f64(&mut cur_res, &x),
+        '/' => calc::divi_mut_f64(&mut cur_res, &x),
+        _ => println!("{} is not a valid operator!", user_input.operator),
     }
     println!("{}", cur_res);
 

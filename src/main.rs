d0164388e2d8a7a9b164e4e2a0b7c4788bb1b4cc
chrnigi <147508181+chrnigi@users.noreply.github.com>
2023-10-30 19:41:36 +0100

    Formatting

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,16 @@ mod input;
 
 fn main() {
     let mut cur_res: f64 = 0.0;
-    
     let mut user_input: input::UserIn;
     
     println!("Current result: {:.3}", cur_res);
 
     loop {
-            user_input = input::num_in();
+        user_input = input::num_in();
             
-            if user_input.operator == 'q' {
-                break;
-            }
+        if user_input.operator == 'q' {
+            break;
+        }
         calc::do_next_op(&mut cur_res, &user_input);
         println!("Current result: {:.3}", cur_res);
 

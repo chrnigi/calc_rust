bab6d181d3468db753ea409ac4de80a055def800
chrnigi <147508181+chrnigi@users.noreply.github.com>
2023-10-30 19:14:04 +0100

    sqrt

--- src/calc.rs
+++ src/calc.rs
@@ -1,3 +1,4 @@
+
 use crate::input::UserIn;
 
 fn add_mut_f64(num_1: &mut f64, num_2: &f64) {
@@ -16,12 +17,17 @@ fn divi_mut_f64(num_1: &mut f64, num_2: &f64) {
     *num_1 = *num_1 / *num_2;
 }
 
+fn square_root_f64(num: &mut f64) {
+    *num = f64::sqrt(*num);
+}
+
 pub fn do_next_op(mut res: &mut f64, user_input: &UserIn) {
     match user_input.operator {
         '+' => add_mut_f64(&mut res, &user_input.operand),
         '-' => subtract_mut_f64(&mut res, &user_input.operand),
         '*' => mult_mut_f64(&mut res, &user_input.operand),
         '/' => divi_mut_f64(&mut res, &user_input.operand),
+        '#' => square_root_f64(&mut res),
         _ => println!("{} is not a valid operator!", user_input.operator),
     }
 }
--- src/input.rs
+++ src/input.rs
@@ -17,14 +17,17 @@ pub fn num_in() -> UserIn {
         .expect("Invalid input!");
     
     temp_op = input_op[0..1].trim().parse::<char>().expect("Failed parse!");
-    if temp_op == 'q' {
-        temp_num = 0.0;
-    } else {
-        temp_num = input_op[1..input_op.len()].trim().parse::<f64>().expect("Failed parse!");
+    
+    match temp_op {
+        
+        'q' => temp_num = 0.0,
+        '#' => temp_num = 0.0,
+        '%' => temp_num = 0.0,
+        '!' => temp_num = 0.0,
+        _ => temp_num = input_op[1..input_op.len()].trim().parse::<f64>().expect("Failed parse!"),
+
     }
-   
-    
-    
+        
     let user_input = UserIn { operator: temp_op, operand: temp_num};
     
     return user_input;

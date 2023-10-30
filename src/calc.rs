d08e092c6b4569e896c73da8d33252dfa14fa5ec
chrnigi <147508181+chrnigi@users.noreply.github.com>
2023-10-30 18:59:51 +0100

    do_next_op

--- src/calc.rs
+++ src/calc.rs
@@ -1,3 +1,5 @@
+use crate::input::UserIn;
+
 pub fn add_mut_f64(num_1: &mut f64, num_2: &f64) {
     *num_1 = *num_1 + *num_2;
 }
@@ -12,4 +14,14 @@ pub fn mult_mut_f64(num_1: &mut f64, num_2: &f64) {
 
 pub fn divi_mut_f64(num_1: &mut f64, num_2: &f64) {
     *num_1 = *num_1 / *num_2;
+}
+
+pub fn do_next_op(mut res: &mut f64, user_input: &UserIn) {
+    match user_input.operator {
+        '+' => add_mut_f64(&mut res, &user_input.operand),
+        '-' => subtract_mut_f64(&mut res, &user_input.operand),
+        '*' => mult_mut_f64(&mut res, &user_input.operand),
+        '/' => divi_mut_f64(&mut res, &user_input.operand),
+        _ => println!("{} is not a valid operator!", user_input.operator),
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,19 @@ mod calc;
 mod input;
 
 fn main() {
-    let mut x: f64;
     let mut cur_res: f64 = 0.0;
-    //println!("{}", x);
+    
     let mut user_input: input::UserIn;
     
     println!("Current result: {:.3}", cur_res);
 
     loop {
-        user_input = input::num_in();
-        x = user_input.operand;
-        match user_input.operator {
-            '+' => calc::add_mut_f64(&mut cur_res, &x),
-            '-' => calc::subtract_mut_f64(&mut cur_res, &x),
-            '*' => calc::mult_mut_f64(&mut cur_res, &x),
-            '/' => calc::divi_mut_f64(&mut cur_res, &x),
-            'q' => break,
-            _ => println!("{} is not a valid operator!", user_input.operator),
-        }
+            user_input = input::num_in();
+            
+            if user_input.operator == 'q' {
+                break;
+            }
+        calc::do_next_op(&mut cur_res, &user_input);
         println!("Current result: {:.3}", cur_res);
 
     }

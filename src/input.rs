851df4e72d4135f927419378c033d5432bb0b8dc
chrnigi <147508181+chrnigi@users.noreply.github.com>
2023-10-30 11:06:12 +0100

    update input

--- src/input.rs
+++ src/input.rs
@@ -1,11 +1,23 @@
 use std::io;
+use scanf::sscanf;
 
-pub fn num_in() -> f64 {
+pub struct UserIn {
+    pub operator: char,
+    pub operand: f64,
+}
+
+pub fn num_in() -> UserIn {
     let mut input: String = String::new();
     
+
+    let mut temp_op: char = '0';
+    let mut temp_num: f64 = 0.0;
     io::stdin()
         .read_line(&mut input)
         .expect("Invalid number!");
-    let input = input.trim().parse::<f64>().expect("Failed parse!");
-    return input;
+    sscanf!(&input, "{} {}", temp_op, temp_num);
+    //let input = input.trim().parse::<f64>().expect("Failed parse!");
+    let user_input = UserIn { operator: temp_op, operand: temp_num };
+    
+    return user_input;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
-use std::io;
+
+
 mod calc;
 mod input;
 
 fn main() {
     let mut x = 0.0;
+    let mut cur_res: f64 = 0.0;
     println!("{}", x);
-    let y = input::num_in();
-    calc::add_mut_f64(&mut x, &y);
-    println!("{}", x);
+    let user_input = input::num_in();
+    x = user_input.operand;
+    if user_input.operator == '+'{
+        calc::add_mut_f64(&mut cur_res, &x)
+    }
+    println!("{}", cur_res);
 
 }
